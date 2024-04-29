6b3ebaad5129f532b7227e6d9940d2e9c1bc0a84
Prashant Badal <p26badal@gmail.com>
2024-04-29 14:30:10 +0530

    final

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const express = require('express');
 const errorHandler=require("./middleware/errorHandler");
 const connectDB = require('./config/dbconnection');
 
-// app.get('/api/contacts',(req,res)=>{
-//     res.send("okay message for ")
-// })
+var corsOptions = {
+    origin: 'https://mycontact-backend.onrender.com/',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
 
 connectDB();
 const app=express();
@@ -15,7 +16,7 @@ const app=express();
 const port=process.env.PORT ||5000;
 
 var cors = require('cors') 
-app.use(cors())
+app.use(cors(corsOptions))
 
 app.use(express.json());
 app.get('/hello', (req, res) => {
